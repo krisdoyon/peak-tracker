3913fa2f121ee78b40c83983544a46ad2c363d48
Kris Doyon <kristopher.doyon@gmail.com>
2022-10-19 19:58:44 -0400

    Add generate markup private methods

--- src/js/views/historyView.js
+++ src/js/views/historyView.js
@@ -1,4 +1,5 @@
 class HistoryView {
+  #data;
   #historyEntriesEl = document.querySelector(".history-entries-list");
   #singleHistoryTable = document.querySelector(".single-history__table");
   #singleHistoryLabelNumber = document.querySelector(
@@ -8,87 +9,74 @@ class HistoryView {
 
   // PUBLIC METHODS
 
-  addHandlerViewSingleHistory(handler) {
+  addHandlerShowEntry(handler) {
     this.#historyEntriesEl.addEventListener(
       "click",
       function (e) {
         const clicked = e.target.closest(".btn-view");
         if (!clicked) return;
-        const logID = clicked.closest(".list-entry").dataset.id;
-
-        handler(+logID);
+        const { logId, listId } = clicked.closest(".list-entry").dataset;
+        handler(+logId, listId);
       }.bind(this)
     );
   }
 
-  addHandlerRemoveHistoryEntry(handler) {
+  addHandlerDeleteEntry(handler) {
     this.#historyEntriesEl.addEventListener("click", function (e) {
       const clicked = e.target.closest(".btn-delete-entry");
       if (!clicked) return;
       if (confirm("Are you sure you want to delete this entry?")) {
-        const logID = +clicked.closest(".list-entry").dataset.id;
-        handler(logID);
+        const { logId } = clicked.closest(".list-entry").dataset;
+        handler(+logId);
       }
     });
   }
 
-  displayAllHistoryList(logEntries) {
+  renderAllHistory(data) {
+    this.#data = data;
     this.#historyEntriesEl.innerHTML = "";
-    if (logEntries.length) {
-      logEntries.forEach((entry) => {
-        this.#historyEntriesEl.insertAdjacentHTML(
-          "beforeend",
-          `<li class="list-entry" data-id="${entry.id}">
-              <button class='btn btn--icon btn-delete-entry'><span class="material-icons">
-              delete
-              </span></button>
-
-              <div class="list-entry__info">
-                <h2 class="list-entry__label-primary"><strong>
-                  ${entry.longDate} </strong> - ${entry.peaks.length} ${
-            entry.peaks.length > 1 ? "Peaks" : "Peak"
-          }
-                </h2>
-                <span
-                  class="list-entry__label-secondary"
-                >
-                  ${entry.mtnStr}</span
-                >
-              </div>
-              
-              <button class="btn btn--dark btn-view btn-view-history">VIEW</button>
-            </li>`
-        );
-      });
+    if (this.#data.length) {
+      this.#historyEntriesEl.insertAdjacentHTML(
+        "beforeend",
+        this.#generatePreviewMarkup()
+      );
     }
   }
 
-  displaySingleHistory(logEntry) {
+  renderHistoryEntry(entry) {
     this.#singleHistoryTable.innerHTML = "";
-    this.#singleHistoryLabelNumber.textContent = `${logEntry.peaks.length} ${
-      logEntry.peaks.length > 1 ? "Peaks" : "Peak"
+    this.#singleHistoryLabelNumber.textContent = `${entry.peaks.length} ${
+      entry.peaks.length > 1 ? "Peaks" : "Peak"
     }`;
-    this.#singleHistoryHeading.textContent = `${logEntry.longDate}`;
+    this.#singleHistoryHeading.textContent = `${entry.longDate}`;
+    this.#singleHistoryTable.insertAdjacentHTML(
+      "beforeend",
+      this.#generateHistoryTableMarkup(entry)
+    );
+  }
+
+  // PRIVATE METHODS
+
+  #generateHistoryTableMarkup(entry) {
     let peaksStr = "";
-    logEntry.peaks.forEach(
+    entry.peaks.forEach(
       (peak) =>
         (peaksStr += `<span style='font-size:1.4rem'>${
           peak.name
         } - ${peak.elevation.toLocaleString()} ft</span>`)
     );
-    let ratingHTML = "";
-    if (logEntry.rating) {
-      for (let i = 0; i < logEntry.rating; i++) {
-        ratingHTML += `<span class="material-icons star-icon--full"> star </span>`;
+    let ratingMarkup = "";
+    if (entry.rating) {
+      for (let i = 0; i < entry.rating; i++) {
+        ratingMarkup += `<span class="material-icons star-icon--full"> star </span>`;
       }
-      for (let i = 0; i < 5 - logEntry.rating; i++) {
-        ratingHTML += `<span class="material-icons"> star_border </span>`;
+      for (let i = 0; i < 5 - entry.rating; i++) {
+        ratingMarkup += `<span class="material-icons"> star_border </span>`;
       }
     } else {
-      ratingHTML = "n/a";
+      ratingMarkup = "n/a";
     }
-
-    const historyHTML = ` <tr>
+    const markup = ` <tr>
         <th style="width: 40%">Peaks:</th>
         <td style="width: 60%">
           <div class="flex-column">
@@ -98,45 +86,74 @@ class HistoryView {
       </tr>
       <tr>
         <th>Distance:</th>
-        <td>${logEntry.distance ? logEntry.distance + ` mi` : "n/a"}</td>
+        <td>${entry.distance ? entry.distance + ` mi` : "n/a"}</td>
       </tr>
       <tr>
         <th>Elevation Gain:</th>
         <td>${
-          logEntry.elevation
-            ? logEntry.elevation.toLocaleString() + ` ft`
-            : "n/a"
+          entry.elevation ? entry.elevation.toLocaleString() + ` ft` : "n/a"
         }</td>
       </tr>
       <tr>
         <th>Time:</th>
-        <td>${logEntry.time ? logEntry.time + ` hrs` : "n/a"}</td>
+        <td>${entry.time ? entry.time + ` hrs` : "n/a"}</td>
       </tr>
       <tr>
         <th>Avg. Speed:</th>
-        <td>${logEntry.avgSpeed ? logEntry.avgSpeed + ` mi/hr` : "n/a"}</td>
+        <td>${entry.avgSpeed ? entry.avgSpeed + ` mi/hr` : "n/a"}</td>
       </tr>
       <tr>
         <th>Avg. Elevation Gain:</th>
         <td>${
-          logEntry.avgElevation
-            ? logEntry.avgElevation.toLocaleString() + ` ft/mi`
+          entry.avgElevation
+            ? entry.avgElevation.toLocaleString() + ` ft/mi`
             : "n/a"
         } </td>
       </tr>
 
       <th>Rating</th>
       <td>
-        ${ratingHTML}
+        ${ratingMarkup}
       </td>
       <tr>
         <th>Notes:</th>
         <td>
-          ${logEntry.notes ? logEntry.notes : "n/a"}
+          ${entry.notes ? entry.notes : "n/a"}
         </td>
       </tr>`;
+    return markup;
+  }
 
-    this.#singleHistoryTable.insertAdjacentHTML("beforeend", historyHTML);
+  #generatePreviewMarkup() {
+    return this.#data
+      .map((entry) => this.#generateSinglePreviewMarkup(entry))
+      .join("");
+  }
+
+  #generateSinglePreviewMarkup(entry) {
+    const markup = `<li class="list-entry" data-log-id="${
+      entry.logID
+    }" data-list-id="${entry.listID}">
+              <button class='btn btn--icon btn-delete-entry'><span class="material-icons">
+              delete
+              </span></button>
+
+              <div class="list-entry__info">
+                <h2 class="list-entry__label-primary"><strong>
+                  ${entry.longDate} </strong> - ${entry.peaks.length} ${
+      entry.peaks.length > 1 ? "Peaks" : "Peak"
+    }
+                </h2>
+                <span
+                  class="list-entry__label-secondary"
+                >
+                  ${entry.mtnStr}</span
+                >
+              </div>
+              
+              <button class="btn btn--dark btn-view btn-view-history">VIEW</button>
+            </li>`;
+    return markup;
   }
 }
 
