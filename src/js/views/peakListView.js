7973318bda0dbe035ea2635df8f47d16e4f42410
Kris Doyon <kristopher.doyon@gmail.com>
2022-10-19 20:02:38 -0400

    Add generate markup methods

--- src/js/views/peakListView.js
+++ src/js/views/peakListView.js
@@ -1,4 +1,7 @@
 class PeakListsView {
+  #previewData;
+  #tableData;
+  #previewType;
   #peakListsEl = document.querySelector(".peak-lists");
   #singleListContainer = document.querySelector(".container-single-peak-list");
   #singleListHeading = this.#singleListContainer.querySelector(
@@ -22,136 +25,141 @@ class PeakListsView {
       function (e) {
         const clicked = e.target.closest(".btn-log-trip");
         if (!clicked) return;
-        const listID = clicked.dataset.listId;
-        const mtnID = clicked.dataset.mtnId;
-        handler(listID, mtnID);
+        const { listId, mtnId } = clicked.dataset;
+        handler(listId, mtnId);
       }.bind(this)
     );
   }
 
-  addHandlerSaveList(handler) {
+  addHandlerSavedLists(handler) {
     this.#peakListsEl.addEventListener("click", function (e) {
       const clicked = e.target.closest(".btn-add-peak-list");
       if (!clicked) return;
-      handler(clicked.dataset.id);
+      const { listId } = e.target.closest(".list-entry").dataset;
+      handler(listId);
     });
   }
 
-  addPeakListViewHandler(handler) {
+  addHandlerPeakListView(handler) {
     this.#peakListsEl.addEventListener("click", function (e) {
-      const listID = e.target.closest(".list-entry").dataset.id;
       const clicked = e.target.closest(".btn-view-list");
       if (!clicked) return;
-      handler(listID);
+      const { listId } = e.target.closest(".list-entry").dataset;
+      handler(listId);
     });
   }
 
-  addHandlerBtnsWrapper(handler) {
+  addHandlerPeakListPreview(handler) {
     this.#peakListBtnsWrapper.addEventListener(
       "click",
       function (e) {
         const clicked = e.target.closest(".btn--text");
         if (!clicked) return;
-        const type = clicked.dataset.display;
-        handler(type);
+        const { previewType } = clicked.dataset;
+        handler(previewType);
       }.bind(this)
     );
   }
 
-  displayPeakListsPreview(listArr, type, savedLists, listCounts) {
-    this.#setActivePreviewBtn(type);
+  renderPeakListsPreview(data) {
+    this.#previewData = data.data;
+    this.#previewType = data.previewType;
+    this.#setActivePreviewBtn(this.#previewType);
     this.#peakListsEl.innerHTML = "";
-    listArr.forEach((list) => {
-      this.#peakListsEl.insertAdjacentHTML(
-        "beforeend",
-        this.#generatePreviewMarkup(list, savedLists, listCounts)
-      );
-    });
+    this.#peakListsEl.insertAdjacentHTML(
+      "beforeend",
+      this.#generatePreviewMarkup(data)
+    );
   }
 
-  displaySinglePeakList(list, listCounts, logEntries) {
-    this.#singleListHeading.textContent = `${list.title}`;
-    this.#singleListNumberLabel.textContent = `${listCounts[list.id]} of ${
-      list.peakCount
-    } Peaks`;
+  renderPeakListTable(data) {
+    this.#tableData = data;
+    this.#singleListHeading.textContent = `${this.#tableData.title}`;
+    this.#singleListNumberLabel.textContent = `${
+      this.#tableData.numCompleted
+    } of ${this.#tableData.peakCount} Peaks`;
     this.#singleListProgressWrapper.innerHTML = "";
     this.#singleListProgressWrapper.insertAdjacentHTML(
       "beforeend",
-      this.#generateProgressBarHTML(list, listCounts)
+      this.#generateProgressBarMarkup(this.#tableData)
     );
     this.#peakListTableBody.innerHTML = "";
     this.#peakListTableBody.insertAdjacentHTML(
       "beforeend",
-      this.#generateSinglePeakListHTML(list, logEntries)
+      this.#generateTableMarkup()
     );
   }
 
   // PRIVATE METHODS
 
-  #setActivePreviewBtn(type) {
-    this.#previewBtns.forEach((btn) => btn.classList.remove("btn--active"));
-    const activeBtn = this.#previewBtns.find(
-      (btn) => btn.dataset.display === type
-    );
-    activeBtn.classList.add("btn--active");
+  #generateTableMarkup() {
+    return this.#tableData.peaks
+      .map((peak) => this.#generateTableRowMarkup(peak))
+      .join("");
   }
 
-  #generateSinglePeakListHTML(list, logEntries) {
-    let peakListHTML = "";
-    list.data.forEach((peak, i) => {
-      let logMatch;
-      if (logEntries.length) {
-        logMatch = logEntries.find((entry) =>
-          entry.peaks.some((peakObj) => peakObj.id === peak.id)
-        );
-      }
-
-      const htmlRow = `<tr class="peak-list__table-row ${
-        logMatch && "peak-list__table-row--complete"
-      }" data-mtn-id="${peak.id}" data-list-id="${peak.id}">
-        <td>${i + 1}</td>
+  #generateTableRowMarkup(peak) {
+    const markup = `<tr class="peak-list__table-row ${
+      peak.completed && "peak-list__table-row--complete"
+    }" data-mtn-id="${peak.id}" data-list-id="${peak.id}">
+        <td>${peak.num}</td>
         <td style="text-align:left">${peak.name}</td>
         <td>${peak.state}</td>
 
-        <td>${peak.elevFeet.toLocaleString()}</td>
+        <td>${peak.elevation.toLocaleString()}</td>
         <td>${
-          logMatch
-            ? `${logMatch.shortDate}`
-            : `<button class='btn btn--text btn-log-trip' data-mtn-id='${peak.id}' data-list-id='${list.id}'>LOG TRIP</button>`
+          peak.completed
+            ? `${peak.completedDate}`
+            : `<button class='btn btn--text btn-log-trip' data-mtn-id='${
+                peak.id
+              }' data-list-id='${this.#tableData.listID}'>LOG TRIP</button>`
         }</td>
       </tr>`;
-      peakListHTML += htmlRow;
-    });
-    return peakListHTML;
+    return markup;
   }
 
-  #generatePreviewMarkup(list, savedLists, listCounts) {
-    const markup = `<li class="list-entry" data-id="${list.id}">
-        <button class="btn btn--icon btn-add-peak-list" data-id='${list.id}'>
+  #generatePreviewMarkup() {
+    return this.#previewData
+      .map((list) => this.#generateSinglePreviewMarkup(list))
+      .join("");
+  }
+
+  #generateSinglePreviewMarkup(list) {
+    const markup = `<li class="list-entry" data-list-id="${list.listID}">
+        <button class="btn btn--icon btn-add-peak-list" data-id='${
+          list.listID
+        }'>
         <span class="material-icons"> ${
-          savedLists.includes(list.id) ? "remove_circle_outline" : "add_circle"
+          list.saved ? "remove_circle_outline" : "add_circle"
         } </span>
       </button>
       <div class="list-entry__info">
         <h2 class="list-entry__label-primary"><strong>${
           list.title
         }</strong></h2>
-        <span class="list-entry__label-secondary">${listCounts[list.id]} of ${
+        <span class="list-entry__label-secondary">${list.numCompleted} of ${
       list.peakCount
     } Peaks</span>
-        ${this.#generateProgressBarHTML(list, listCounts)}
+        ${this.#generateProgressBarMarkup(list)}
         <button class="btn btn--dark btn-view btn-view-list">VIEW</button>
     </li>`;
     return markup;
   }
 
-  #generateProgressBarHTML(list, listCounts) {
-    const width = (listCounts[list.id] / list.data.length) * 100;
-    const html = `<div class='progress-bar'><div class='progress-bar__label'>${
+  #generateProgressBarMarkup(list) {
+    const width = (list.numCompleted / list.peakCount) * 100;
+    const markup = `<div class='progress-bar'><div class='progress-bar__label'>${
       Math.round(width * 10) / 10
     }%</div><div class='progress' style="width:${width}%"></div></div>
       </div>`;
-    return html;
+    return markup;
+  }
+
+  #setActivePreviewBtn() {
+    this.#previewBtns.forEach((btn) => btn.classList.remove("btn--active"));
+    this.#previewBtns
+      .find((btn) => btn.dataset.previewType === this.#previewType)
+      .classList.add("btn--active");
   }
 }
 
