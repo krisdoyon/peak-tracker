2c4db5e665330ce4bf806ee6baa82c749749a7f4
Kris Doyon <kristopher.doyon@gmail.com>
2022-10-27 00:05:58 -0400

    Add handler for page load

--- src/js/views/mainView.js
+++ src/js/views/mainView.js
@@ -1,40 +1,27 @@
-import icons from "../../img/sprite.svg";
-
 class MainView {
   #containerMain = document.querySelector(".container-main");
-  #allContainers = [...document.querySelectorAll(".container")];
   #btnCloseContainer = this.#containerMain.querySelector(".btn-close");
-  #btnLoadData = document.querySelector("#btn-load-data-map");
-  #btnClearData = document.querySelector("#btn-clear-data-map");
-  #mainNavBtns = document.querySelectorAll(".main-nav__btn");
-  #mainNavList = document.querySelector(".main-nav__list");
-  #sidebar = document.querySelector(".sidebar");
-  #btnSidebar = document.querySelector(".sidebar__btn");
-  #btnSidebarIcon = this.#btnSidebar.querySelector("use");
-  #btnAbout = document.querySelector(".btn-about");
+  #allContainers = document.querySelectorAll(".container");
+  #mapNavBtn = document.querySelector("#nav-btn-map");
+  #allNavBtns = document.querySelectorAll(".nav__btn");
 
   constructor() {
     this.#addHandlerEscapeKeydown();
+    this.#addHandlerRemovePreload();
   }
 
   // PUBLIC METHODS
 
-  addHandlerLoadPage(handler) {
-    window.addEventListener(
-      "load",
-      function (e) {
-        const containerID = window.location.pathname.slice(1) || "map";
-        this.#containerMain.classList.remove("preload");
-        this.#sidebar.classList.remove("preload");
-        handler(containerID);
-      }.bind(this)
-    );
-  }
-
-  addHandlerHideContainer(handler) {
-    this.#btnCloseContainer.addEventListener("click", function () {
+  addHandlerPageLoad(handler) {
+    window.addEventListener("load", function () {
+      window.location.pathname.slice(1) ||
+        this.window.history.replaceState(null, "", "/map");
       handler();
     });
+  }
+
+  addHandlerCloseContainer(handler) {
+    this.#btnCloseContainer.addEventListener("click", handler);
   }
 
   addHandlerBtnBack(handler) {
@@ -45,73 +32,17 @@ class MainView {
     });
   }
 
-  addHandlerMainNav(handler) {
-    this.#mainNavList.addEventListener("click", function (e) {
-      const clicked = e.target.closest(".main-nav__btn");
-      if (!clicked) return;
-      const containerID = clicked.dataset.display;
-      handler(containerID);
-    });
-  }
-
-  addHandlerLoadData(handler) {
-    this.#btnLoadData.addEventListener("click", handler);
-  }
-
-  addHandlerClearAllData(handler) {
-    this.#btnClearData.addEventListener("click", handler);
-  }
-
-  addHandlerBtnAbout(handler) {
-    this.#btnAbout.addEventListener("click", handler);
-  }
-
-  showContainer(containerID) {
-    window.history.pushState(null, "", `/${containerID}`);
-    const containerObj = this.#allContainers.find((container) =>
-      container.classList.contains(`container-${containerID}`)
-    );
-    this.#allContainers.forEach((container) =>
-      container.classList.add("hidden")
-    );
-    this.#containerMain.classList.remove("hidden");
-    containerObj.classList.remove("hidden");
-    this.#mainNavBtns.forEach((btn) => {
-      btn.classList.remove("main-nav__btn--active");
-      btn.dataset.display === containerObj.dataset.navActive &&
-        btn.classList.add("main-nav__btn--active");
-    });
-  }
-
-  hideContainer() {
-    window.history.pushState(null, "", `/map`);
+  closeContainer() {
+    window.history.replaceState(null, "", "/map");
     this.#containerMain.classList.add("hidden");
     this.#allContainers.forEach((container) =>
       container.classList.add("hidden")
     );
-    this.#mainNavBtns.forEach((btn) => {
-      btn.classList.remove("main-nav__btn--active");
-      btn.dataset.display === "map" &&
-        btn.classList.add("main-nav__btn--active");
-    });
+    this.#allNavBtns.forEach((btn) => btn.classList.remove("nav__btn--active"));
+    this.#mapNavBtn.classList.add("nav__btn--active");
   }
 
   // PRIVATE METHODS
-
-  addHandlerSidebar(handler) {
-    this.#btnSidebar.addEventListener("click", handler);
-  }
-
-  toggleSidebar(sidebarHidden) {
-    [this.#sidebar, this.#containerMain].forEach(
-      (element) =>
-        (element.style.marginLeft = `${sidebarHidden ? "-18.2rem" : "0"}`)
-    );
-    this.#btnSidebarIcon.setAttribute(
-      "href",
-      `${icons}#icon-${sidebarHidden ? "chevron-right" : "chevron-left"}`
-    );
-  }
 
   #addHandlerEscapeKeydown() {
     document.addEventListener(
@@ -119,7 +50,16 @@ class MainView {
       function (e) {
         e.key === "Escape" &&
           !this.#containerMain.classList.contains("hidden") &&
-          this.closeMainContainer();
+          this.closeContainer();
+      }.bind(this)
+    );
+  }
+
+  #addHandlerRemovePreload() {
+    window.addEventListener(
+      "load",
+      function () {
+        this.#containerMain.classList.remove("preload");
       }.bind(this)
     );
   }
