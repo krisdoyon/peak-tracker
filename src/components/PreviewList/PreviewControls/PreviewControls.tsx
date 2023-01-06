3c35fe083f090d4e88a674a4bd2ee5786251d004
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-05 19:45:42 -0500

    Add PreviewControls component

---
+++ src/components/PreviewList/PreviewControls/PreviewControls.tsx
@@ -0,0 +1,24 @@
+import { FilterSelects } from "components/FilterSelects/FilterSelects";
+import styles from "./PreviewControls.module.scss";
+
+interface Props {
+  children?: React.ReactNode;
+  variant: "log" | "peak-lists" | "stats";
+}
+
+export const PreviewControls = ({ children, variant }: Props) => {
+  return (
+    <div
+      className={`${styles.wrapper} ${
+        variant === "peak-lists" ? styles["peak-lists"] : ""
+      }${variant === "log" || variant === "stats" ? styles.selects : ""}
+      ${variant === "log" ? styles.log : ""}
+      ${variant === "stats" ? styles.stats : ""}`}
+    >
+      {(variant === "log" || variant === "stats") && (
+        <FilterSelects card={variant} />
+      )}
+      {children}
+    </div>
+  );
+};
