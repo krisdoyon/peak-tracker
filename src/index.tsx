e503e817ade2a879f5084e277cf3a29ea3c51a4d
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-13 21:21:00 -0500

    Add context providers to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import { LogProvider } from "context/logContext";
+import { StatsProvider } from "context/statsContext";
+import { PeakListProvider } from "context/peakListContext";
+import { NewEntryProvider } from "context/newEntryContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <LogProvider>
+      <StatsProvider>
+        <PeakListProvider>
+          <NewEntryProvider>
+            <App />
+          </NewEntryProvider>
+        </PeakListProvider>
+      </StatsProvider>
+    </LogProvider>
   </React.StrictMode>
 );
