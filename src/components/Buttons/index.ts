50fec83d043f584e084f363171054dc64c909e01
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-13 21:10:23 -0500

    Forward button components from index

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -1,3 +1,6 @@
 export { IconButton } from "./IconButton/IconButton";
 export { TextButton } from "./TextButton/TextButton";
 export { Button } from "./Button/Button";
+export { ViewButton } from "./ViewButton/ViewButton";
+export { PaginationButton } from "./PaginationButton/PaginationButton";
+export { AddButton } from "./AddButton/AddButton";
