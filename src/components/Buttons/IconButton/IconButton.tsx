2b1f9f79c198e56ca14a5ec105acabfd378bb8c2
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-13 21:09:39 -0500

    Add onClick and className to IconButton props

--- src/components/Buttons/IconButton/IconButton.tsx
+++ src/components/Buttons/IconButton/IconButton.tsx
@@ -3,12 +3,23 @@ import sprite from "assets/img/sprite.svg";
 
 interface Props {
   icon: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  small?: boolean;
 }
 
-export const IconButton = ({ icon }: Props) => {
+export const IconButton = ({
+  icon,
+  onClick,
+  className,
+  small = false,
+}: Props) => {
   return (
-    <button className={styles.btn}>
-      <svg className={styles.icon}>
+    <button
+      className={`${styles.btn} ${className ? className : ""}`}
+      onClick={onClick}
+    >
+      <svg className={`${styles.icon} ${small ? styles.small : ""}`}>
         <use href={`${sprite}#icon-${icon}`}></use>
       </svg>
     </button>
