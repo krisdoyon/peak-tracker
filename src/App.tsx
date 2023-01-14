3181ab1055b9d565617779f862d62e2c097e6e0c
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-13 21:20:20 -0500

    Add single peak list and single log entry routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.module.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import { Layout } from "./layout/Layout/Layout";
 import { PeakLists, Log, Stats, NewEntry } from "./pages";
+import { SinglePeakList } from "pages/SinglePeakList/SinglePeakList";
+import { SingleLogEntry } from "pages/SingleLogEntry/SingleLogEntry";
 
 function App() {
   return (
@@ -10,7 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/peak-lists" element={<PeakLists />}></Route>
+          <Route path="/peak-lists/:listID" element={<SinglePeakList />} />
           <Route path="/log" element={<Log />}></Route>
+          <Route path="/log/:logID" element={<SingleLogEntry />} />
           <Route path="/stats" element={<Stats />}></Route>
           <Route path="/new-entry" element={<NewEntry />}></Route>
         </Route>
