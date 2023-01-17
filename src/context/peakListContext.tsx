0993a6858b86cd7e524683cc012cf8b5fe50a494
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-16 22:05:50 -0500

    Add util functions to peak list context

--- src/context/peakListContext.tsx
+++ src/context/peakListContext.tsx
@@ -1,47 +1,194 @@
-import { IPeakList } from "models/interfaces";
-import { createContext, SetStateAction, useContext, useState } from "react";
+import { ILogEntry, IPeak, IPeakList } from "models/interfaces";
+import { createContext, useContext, useMemo, useReducer } from "react";
 import { peakListsArr } from "assets/data/createPeakLists";
+import { useLogContext } from "./logContext";
 
 interface Props {
   children: React.ReactNode;
 }
 
-interface IPeakListContext {
-  allPeakLists: IPeakList[];
-  getPeakListById: (listID: string) => IPeakList | undefined;
-  savedListIds: string[];
-  toggleSavedList: (listID: string) => void;
+export enum PeakListActionType {
+  TOGGLE_SAVED_LIST,
+  SET_COMPLETED_PEAKS,
+  SET_LIST_COUNTS,
 }
 
-const getPeakListById = (listID: string) => {
-  const list = peakListsArr.find((list) => list.listID === listID);
-  return list;
+interface IPeakListState {
+  allPeakLists: IPeakList[];
+  savedListIds: string[];
+  completedPeaks: number[];
+  listCounts: {
+    [listID: string]: number;
+  };
+}
+
+type ToggleSavedList = {
+  type: PeakListActionType.TOGGLE_SAVED_LIST;
+  payload: string;
 };
+
+type SetCompletedPeaks = {
+  type: PeakListActionType.SET_COMPLETED_PEAKS;
+  payload: number[];
+};
+
+type SetListCounts = {
+  type: PeakListActionType.SET_LIST_COUNTS;
+  payload: { [listId: string]: number };
+};
+
+type PeakListAction = ToggleSavedList | SetCompletedPeaks | SetListCounts;
+
+interface IPeakListContext {
+  state: IPeakListState;
+  dispatch: React.Dispatch<PeakListAction>;
+  getPeakListById: (listID: string) => IPeakList | undefined;
+  isPeakCompleted: (peakID: number) => boolean;
+  setCompletedPeaks: (logEntries: ILogEntry[]) => void;
+  getPeakNames: (peakIds: number[]) => (string | undefined)[];
+  getMatchingListIds: (peakId: number) => string[];
+  calculateListCounts: () => void;
+  getListTitleById: (listID: string) => string | undefined;
+  getLogListIds: (peakIds: number[]) => string[];
+  getPeakById: (peakId: number) => IPeak | undefined;
+}
 
 const peakListContext = createContext<IPeakListContext | null>(null);
 
-export const PeakListProvider = ({ children }: Props) => {
-  const [savedListIds, setSavedListIds] = useState(["nh4k"]);
+const initialState = {
+  allPeakLists: peakListsArr,
+  savedListIds: ["nh4k", "adk46"],
+  completedPeaks: [],
+  listCounts: {},
+};
 
-  const toggleSavedList = (listID: string) => {
-    if (savedListIds.some((savedID) => savedID === listID)) {
-      setSavedListIds((prevSavedIds) =>
-        prevSavedIds.filter((savedId) => savedId !== listID)
-      );
+const peakListReducer = (state: IPeakListState, action: PeakListAction) => {
+  if (action.type === PeakListActionType.TOGGLE_SAVED_LIST) {
+    const listID = action.payload;
+    let newListIds;
+    if (state.savedListIds.some((savedID) => savedID === listID)) {
+      newListIds = state.savedListIds.filter((savedId) => savedId !== listID);
     } else {
-      setSavedListIds([...savedListIds, listID]);
+      newListIds = [...state.savedListIds, listID];
     }
+    return { ...state, savedListIds: newListIds };
+  }
+  if (action.type === PeakListActionType.SET_COMPLETED_PEAKS) {
+    return { ...state, completedPeaks: action.payload };
+  }
+  if (action.type === PeakListActionType.SET_LIST_COUNTS) {
+    return { ...state, listCounts: action.payload };
+  }
+  return { ...state };
+};
+
+export const PeakListProvider = ({ children }: Props) => {
+  const [state, dispatch] = useReducer(peakListReducer, initialState);
+
+  const getPeakListById = (listID: string) => {
+    const list = state.allPeakLists.find((list) => list.listID === listID);
+    return list;
   };
 
+  const isPeakCompleted = (peakID: number) => {
+    return state.completedPeaks.includes(peakID);
+  };
+
+  const setCompletedPeaks = (logEntries: ILogEntry[]) => {
+    const uniquePeaksArr = [
+      ...new Set(logEntries.flatMap((entry) => entry.peakIds)),
+    ];
+    dispatch({
+      type: PeakListActionType.SET_COMPLETED_PEAKS,
+      payload: uniquePeaksArr,
+    });
+  };
+
+  const getAllUniquePeaks = () => {
+    return [
+      ...new Set(state.allPeakLists.flatMap((peakList) => peakList.peaks)),
+    ];
+  };
+
+  const getMatchingListIds = (peakID: number) => {
+    const listMatchIds: string[] = [];
+    state.allPeakLists.forEach((peakList) => {
+      if (peakList.peaks.some((peak) => peak.id === peakID)) {
+        listMatchIds.push(peakList.listID);
+      }
+    });
+    return listMatchIds;
+  };
+
+  const getLogListIds = (peakIds: number[]) => {
+    return [
+      ...new Set(peakIds.flatMap((peakId) => getMatchingListIds(peakId))),
+    ];
+  };
+
+  const getPeakNames = (peakIdsArr: number[]) => {
+    const uniquePeaks = getAllUniquePeaks();
+    const peakNamesArr = peakIdsArr.map((peakId) => {
+      const peakMatch = uniquePeaks.find((peak) => peak.id === peakId);
+      return peakMatch?.name;
+    });
+    return peakNamesArr;
+  };
+
+  const calculateListCounts = () => {
+    const listCounts: { [listID: string]: number } = {};
+    state.allPeakLists.forEach((peakList) => {
+      listCounts[peakList.listID] = 0;
+    });
+    state.completedPeaks.forEach((peakID) => {
+      const listMatchIds = getMatchingListIds(peakID);
+      listMatchIds.forEach((listID) => {
+        listCounts[listID]++;
+      });
+    });
+    dispatch({ type: PeakListActionType.SET_LIST_COUNTS, payload: listCounts });
+  };
+
+  const getListTitleById = (listID: string) => {
+    const listMatch = state.allPeakLists.find((list) => list.listID === listID);
+    return listMatch?.title;
+  };
+
+  const getPeakById = (peakId: number) => {
+    const uniquePeaks = getAllUniquePeaks();
+    return uniquePeaks.find((peak) => peak.id === peakId);
+  };
+
+  const contextValue = useMemo(() => {
+    return {
+      state,
+      dispatch,
+      getPeakListById,
+      setCompletedPeaks,
+      isPeakCompleted,
+      getPeakNames,
+      getMatchingListIds,
+      calculateListCounts,
+      getListTitleById,
+      getLogListIds,
+      getPeakById,
+    };
+  }, [
+    state,
+    dispatch,
+    getPeakListById,
+    setCompletedPeaks,
+    isPeakCompleted,
+    getPeakNames,
+    getMatchingListIds,
+    calculateListCounts,
+    getListTitleById,
+    getLogListIds,
+    getPeakById,
+  ]);
+
   return (
-    <peakListContext.Provider
-      value={{
-        allPeakLists: peakListsArr,
-        getPeakListById,
-        savedListIds,
-        toggleSavedList,
-      }}
-    >
+    <peakListContext.Provider value={contextValue}>
       {children}
     </peakListContext.Provider>
   );
