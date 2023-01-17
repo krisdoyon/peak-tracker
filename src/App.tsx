3fb24cea83afb55ab5b7bd1bda034d6aaffddf70
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-16 22:11:27 -0500

    Add useEffects to update completed peaks and list counts when log entries change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "./layout/Layout/Layout";
 import { PeakLists, Log, Stats, NewEntry } from "./pages";
 import { SinglePeakList } from "pages/SinglePeakList/SinglePeakList";
 import { SingleLogEntry } from "pages/SingleLogEntry/SingleLogEntry";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useNewEntryContext } from "context/newEntryContext";
+import {
+  PeakListActionType,
+  usePeakListContext,
+} from "context/peakListContext";
+import { useLogContext } from "context/logContext";
 
 function App() {
+  const { pathname } = useLocation();
+  const { state, dispatch } = useNewEntryContext();
+  useEffect(() => {
+    // If form is not empty, confirm navigate away
+  }, [pathname]);
+
+  const {
+    state: { logEntries },
+  } = useLogContext();
+
+  const {
+    state: { completedPeaks },
+    setCompletedPeaks,
+    calculateListCounts,
+  } = usePeakListContext();
+
+  useEffect(() => {
+    setCompletedPeaks(logEntries);
+  }, [logEntries]);
+
+  useEffect(() => {
+    calculateListCounts();
+  }, [completedPeaks]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/peak-lists" element={<PeakLists />}></Route>
-          <Route path="/peak-lists/:listID" element={<SinglePeakList />} />
-          <Route path="/log" element={<Log />}></Route>
-          <Route path="/log/:logID" element={<SingleLogEntry />} />
-          <Route path="/stats" element={<Stats />}></Route>
-          <Route path="/new-entry" element={<NewEntry />}></Route>
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/peak-lists" element={<PeakLists />} />
+        <Route path="/peak-lists/:listID" element={<SinglePeakList />} />
+        <Route path="/log" element={<Log />} />
+        <Route path="/log/:logID" element={<SingleLogEntry />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/new-entry" element={<NewEntry />} />
+      </Route>
+    </Routes>
   );
 }
 
