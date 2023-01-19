a55ef82ca4622d4c011c08130ef944c1a344253b
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-18 22:47:32 -0500

    Change ViewButton navigate to prop to onClick

--- src/components/Buttons/ViewButton/ViewButton.tsx
+++ src/components/Buttons/ViewButton/ViewButton.tsx
@@ -1,6 +1,5 @@
 import styles from "./ViewButton.module.scss";
 import { Button } from "components/Buttons";
-import { useNavigate } from "react-router-dom";
 
 interface Props {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
@@ -9,7 +8,6 @@ interface Props {
 }
 
 export const ViewButton = ({ onClick, small = false }: Props) => {
-  const navigate = useNavigate();
   return (
     <Button
       className={`${styles.view}  ${small ? styles.small : ""}`}
