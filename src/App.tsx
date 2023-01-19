8524032e63e71ef4abb6ab1f5b6069a64c067d99
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-18 22:52:55 -0500

    Add useEffect to clear map on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import { Layout } from "./layout/Layout/Layout";
 import { PeakLists, Log, Stats, NewEntry } from "./pages";
 import { SinglePeakList } from "pages/SinglePeakList/SinglePeakList";
 import { SingleLogEntry } from "pages/SingleLogEntry/SingleLogEntry";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useNewEntryContext } from "context/newEntryContext";
+import {
+  NewEntryActionKind,
+  useNewEntryContext,
+} from "context/newEntryContext";
 import {
   PeakListActionType,
   usePeakListContext,
 } from "context/peakListContext";
 import { useLogContext } from "context/logContext";
+import { MapActionType, useMapContext } from "context/mapContext";
 
 function App() {
   const { pathname } = useLocation();
-  const { state, dispatch } = useNewEntryContext();
-  useEffect(() => {
-    // If form is not empty, confirm navigate away
-  }, [pathname]);
 
+  const { state, dispatch: newEntryDispatch } = useNewEntryContext();
+  const {
+    state: { peaks },
+    dispatch: mapDispatch,
+  } = useMapContext();
+
+  console.log({ peaks });
   const {
     state: { logEntries },
   } = useLogContext();
@@ -36,6 +43,13 @@ function App() {
   useEffect(() => {
     calculateListCounts();
   }, [completedPeaks]);
+
+  useEffect(() => {
+    if (!pathname.includes("/peak-lists/") && !pathname.includes("/log/"))
+      mapDispatch({ type: MapActionType.CLEAR_MAP });
+    // newEntryDispatch({ type: NewEntryActionKind.RESET_FORM });
+    // If form is not empty, confirm navigate away
+  }, [pathname]);
 
   return (
     <Routes>
