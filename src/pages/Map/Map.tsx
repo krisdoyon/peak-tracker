9d6e0010f9f650fe1fa2e26d010cb859521d0cd3
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-18 22:50:40 -0500

    Conditionally render PeakLayer component from map

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,23 +1,13 @@
 import styles from "./Map.module.scss";
-import {
-  MapContainer,
-  TileLayer,
-  Popup,
-  Marker,
-  ZoomControl,
-} from "react-leaflet";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
+
+import { PeakLayer } from "./PeakLayer/PeakLayer";
 import { useMapContext } from "context/mapContext";
-import mtnIconGreen from "assets/img/mtn-icon-green.png";
-import mtnIconRed from "assets/img/mtn-icon-red.png";
-import L from "leaflet";
-import { usePeakListContext } from "context/peakListContext";
 
 export const Map = () => {
   const {
     state: { peaks },
   } = useMapContext();
-
-  const { isPeakCompleted } = usePeakListContext();
 
   return (
     <MapContainer
@@ -28,27 +18,7 @@ export const Map = () => {
     >
       <ZoomControl position="bottomright" />
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {peaks.map((peak) => {
-        const isCompleted = isPeakCompleted(peak.id);
-        const icon = new L.Icon({
-          iconUrl: isCompleted ? mtnIconGreen : mtnIconRed,
-          iconSize: [25, 20],
-        });
-
-        return (
-          <Marker
-            key={peak.id}
-            position={[peak.lat, peak.long]}
-            icon={icon}
-            // iconUrl={peak.completed ? mtnIconGreen : mtnIconRed}
-          />
-        );
-      })}
-      {/* <Marker position={[51.505, -0.09]} icon={}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
+      {peaks.length > 0 && <PeakLayer />}
     </MapContainer>
   );
 };
