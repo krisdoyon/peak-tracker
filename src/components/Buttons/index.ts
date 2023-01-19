b4d393a699b1cac26a12a05cc6b8f3c21de43b13
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-18 22:47:58 -0500

    Forward LogTripButton

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -4,3 +4,4 @@ export { Button } from "./Button/Button";
 export { ViewButton } from "./ViewButton/ViewButton";
 export { PaginationButton } from "./PaginationButton/PaginationButton";
 export { AddButton } from "./AddButton/AddButton";
+export { LogTripButton } from "./LogTripButton/LogTripButton";
