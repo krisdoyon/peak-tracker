8745a26dd3ad75faa7721fb01f7ce786edf01c33
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-25 09:38:25 -0500

    Move card with sidebar button press

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,18 @@
+import { useSidebarContext } from "context/sidebarContext";
 import { Link } from "react-router-dom";
 import styles from "./Card.module.scss";
+import sidebarStyles from "layout/Sidebar/Sidebar.module.scss";
 
 interface Props {
   children?: React.ReactNode;
 }
 
 export const Card = ({ children }: Props) => {
+  const { sidebarOpen } = useSidebarContext();
   return (
-    <div className={styles.card}>
+    <div
+      className={`${styles.card} ${sidebarOpen ? "" : sidebarStyles.hidden}`}
+    >
       <Link to="/" className={styles["btn-close"]}>
         &times;
       </Link>
