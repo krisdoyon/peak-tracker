06b01042cb211980aa0e1e7d8a0099080921b53c
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-25 09:36:28 -0500

    Add load test entries button

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import { Sidebar } from "layout/Sidebar/Sidebar";
 import { Logo } from "components/Logo/Logo";
 import { Map } from "pages/Map/Map";
+import { LoadTestButton } from "./loadTestButton";
 
 export const Layout = () => {
   return (
     <main>
       <Logo />
       <Map />
+      <LoadTestButton />
       <Sidebar />
       <Outlet />
     </main>
