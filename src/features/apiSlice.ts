9a7fab5c6b194c114d2b2feaab38e569b092f7c5
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-25 09:39:42 -0500

    Add test log entries query

--- src/features/apiSlice.ts
+++ src/features/apiSlice.ts
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { testLogEntries } from "assets/testLogEntries";
 import { ILogEntry, IPeakList } from "models/interfaces";
 
 export const apiSlice = createApi({
   reducerPath: "api",
+  tagTypes: ["savedLists", "logEntries"],
   baseQuery: fetchBaseQuery({
     baseUrl: "https://peak-tracker-5856f-default-rtdb.firebaseio.com",
   }),
@@ -10,15 +12,109 @@ export const apiSlice = createApi({
     getLists: builder.query<IPeakList[], void>({
       query: () => "/lists.json",
       transformResponse: (res: IPeakList[]) =>
-        res.sort((a, b) => a.title.localeCompare(b.title)),
+        res.sort((a, b) => a.title.localeCompare(b.title)) ?? [],
     }),
     getLogEntries: builder.query<ILogEntry[], string>({
       query: (userId) => `/users/${userId}/logEntries.json`,
-      transformResponse: (res: ILogEntry[]) =>
-        res.filter((entry) => entry != null),
+      transformResponse: (res: ILogEntry[]) => {
+        return Object.keys(res)
+          .map((key) => res[+key])
+          .filter((entry) => entry != null);
+      },
+      providesTags: ["logEntries"],
     }),
     getSavedLists: builder.query<string[], string>({
       query: (userId) => `/users/${userId}/savedLists.json`,
+      transformResponse: (res: string[]) => res ?? [],
+      providesTags: ["savedLists"],
+    }),
+    updateSavedLists: builder.mutation<
+      void,
+      { userId: string; savedLists: string[] }
+    >({
+      query: ({ userId, savedLists }) => ({
+        url: `/users/${userId}/savedLists.json`,
+        method: "PUT",
+        body: savedLists,
+      }),
+      invalidatesTags: ["savedLists"],
+      async onQueryStarted(
+        { userId, savedLists },
+        { dispatch, queryFulfilled }
+      ) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData("getSavedLists", userId, () => {
+            return savedLists;
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+    addLogEntry: builder.mutation<
+      void,
+      { userId: string; newEntry: ILogEntry }
+    >({
+      query: ({ userId, newEntry }) => ({
+        url: `/users/${userId}/logEntries/${newEntry.logID}.json`,
+        method: "PUT",
+        body: newEntry,
+      }),
+      invalidatesTags: ["logEntries"],
+      async onQueryStarted({ userId, newEntry }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData("getLogEntries", userId, (draft) => {
+            return [...draft, newEntry];
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+    removeLogEntry: builder.mutation<void, { userId: string; logId: string }>({
+      query: ({ userId, logId }) => ({
+        url: `/users/${userId}/logEntries/${logId}.json`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["logEntries"],
+      async onQueryStarted({ userId, logId }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData("getLogEntries", userId, (draft) => {
+            return draft.filter((entry) => entry.logID !== logId);
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+    setTestLogEntries: builder.mutation<void, string>({
+      query: (userId) => ({
+        url: `/users/${userId}/logEntries.json`,
+        method: "PUT",
+        body: testLogEntries,
+      }),
+      invalidatesTags: ["logEntries"],
+      async onQueryStarted(userId, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData("getLogEntries", userId, (draft) => {
+            return testLogEntries;
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
     }),
   }),
 });
@@ -27,4 +123,8 @@ export const {
   useGetListsQuery,
   useGetLogEntriesQuery,
   useGetSavedListsQuery,
+  useUpdateSavedListsMutation,
+  useRemoveLogEntryMutation,
+  useAddLogEntryMutation,
+  useSetTestLogEntriesMutation,
 } = apiSlice;
