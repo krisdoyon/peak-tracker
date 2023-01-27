d17dd96298da24f57d42690beaf91c8e8835f876
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-27 17:54:04 -0500

    Add button to plot all peaks

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -3,6 +3,32 @@ import { Sidebar } from "layout/Sidebar/Sidebar";
 import { Logo } from "components/Logo/Logo";
 import { Map } from "pages/Map/Map";
 import { LoadTestButton } from "./loadTestButton";
+import { useGetListsQuery } from "features/apiSlice";
+import { getAllUniquePeaks } from "utils/peakUtils";
+import { Button } from "components/Buttons";
+import { MapActionType, useMapContext } from "context/mapContext";
+
+export const PlotAllButton = () => {
+  const { data: allPeakLists = [] } = useGetListsQuery();
+  const allPeaks = getAllUniquePeaks(allPeakLists);
+  const { dispatch } = useMapContext();
+
+  return (
+    <Button
+      style={{
+        position: "absolute",
+        right: "1.4rem",
+        padding: "1rem 2rem",
+        top: "20rem",
+      }}
+      onClick={() =>
+        dispatch({ type: MapActionType.SET_PEAKS, payload: allPeaks })
+      }
+    >
+      PLOT ALL
+    </Button>
+  );
+};
 
 export const Layout = () => {
   return (
@@ -10,6 +36,7 @@ export const Layout = () => {
       <Logo />
       <Map />
       <LoadTestButton />
+      <PlotAllButton />
       <Sidebar />
       <Outlet />
     </main>
