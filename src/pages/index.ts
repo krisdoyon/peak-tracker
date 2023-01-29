9d9d01b59d4dd10929a96efd7a7bde430e96a4d6
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-28 20:43:00 -0500

    Forward SinglePeakList and SingleLogEntry components from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,5 @@ export { PeakLists } from "./PeakLists/PeakLists";
 export { Log } from "./Log/Log";
 export { NewEntry } from "./NewEntry/NewEntry";
 export { Stats } from "./Stats/Stats";
+export { SinglePeakList } from "./SinglePeakList/SinglePeakList";
+export { SingleLogEntry } from "./SingleLogEntry/SingleLogEntry";
