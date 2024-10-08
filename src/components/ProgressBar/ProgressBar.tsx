fc2843202f7dbc6e66ff104d597a1bdca00c3531
Kris Doyon <kristopher.doyon@gmail.com>
2024-10-08 18:19:35 -0400

    Change color when progress bar reaches 100%

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -7,10 +7,19 @@ interface Props {
 
 export const ProgressBar = ({ peakCount, numCompleted }: Props) => {
   const width = (numCompleted / peakCount) * 100;
+  const isCompleted = width === 100;
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.label}>{`${Math.round(width * 10) / 10}%`}</div>
-      <div className={styles.progress} style={{ width: `${width}%` }}></div>
+      <div
+        className={`${styles.label} ${
+          isCompleted ? styles["label-complete"] : ""
+        }`}
+      >{`${Math.round(width * 10) / 10}%`}</div>
+      <div
+        className={`${styles.progress} ${isCompleted ? styles.complete : ""}`}
+        style={{ width: `${width}%` }}
+      ></div>
     </div>
   );
 };
