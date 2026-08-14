9ec7e206468e9076b7aaa37abf5a4320dc085cab
Kris Doyon <kristopher.doyon@gmail.com>
2026-08-14 12:07:57 -0400

    Switch back to production API URL

--- client/src/assets/config.ts
+++ client/src/assets/config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://peak-tracker-5856f-default-rtdb.firebaseio.com";
-export const API_URL = "http://localhost:8000";
+export const API_URL = "https://peak-tracker-5856f-default-rtdb.firebaseio.com";
+// export const API_URL = "http://localhost:8000";
 export const SIGNUP_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
   import.meta.env.VITE_API_KEY
 }`;
